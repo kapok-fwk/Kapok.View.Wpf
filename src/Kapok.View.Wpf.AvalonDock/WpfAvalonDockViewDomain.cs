45040ce041a183a4c04f6a78c38510d5028dc11e
Leo Schick <leonhard.schick@gmail.com>
2023-08-01 10:27:03 +0200

    set default page window types in constructor of WpfAvalonDockViewDomain

--- src/Kapok.View.Wpf.AvalonDock/WpfAvalonDockViewDomain.cs
+++ src/Kapok.View.Wpf.AvalonDock/WpfAvalonDockViewDomain.cs
@@ -8,6 +8,9 @@ public class WpfAvalonDockViewDomain : WpfViewDomain
     public WpfAvalonDockViewDomain(Action<int> shutdownApplicationAction)
         : base(shutdownApplicationAction)
     {
+        DefaultCardPageWindow = typeof(CardPageWindow);
+        DefaultListPageWindow = typeof(ListPageWindow);
+        DefaultPopupListPageWindow = typeof(PopupListPageWindow);
     }
 
     protected override ContentControl GetPageContentControlFromPageContainerPage(IPage page, IPage pageContainerPage)
